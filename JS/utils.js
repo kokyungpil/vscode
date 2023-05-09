c04942f0afe35429af793f4d7b56ae8cb9a9c408
ko kyungpil <devil1446@naver.com>
2023-05-09 13:06:31 +0900

    실습

---
+++ JS/utils.js
@@ -0,0 +1,18 @@
+// return X , PARAMETER X
+
+function hi(){
+   // alert("HI~!~!")
+    console.log("안녕 난고경필");
+    console.log("");
+}
+
+function sum(x,y) {
+    let result = x + y;
+    console.log(result);
+}
+
+function total(x,y) {
+    return x + y;
+}
+
+
