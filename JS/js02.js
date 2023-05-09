53e7f5d45a643a0d77215b96aeb65b50655b3922
ko kyungpil <devil1446@naver.com>
2023-05-09 17:33:43 +0900

    05-09 수정

---
+++ JS/js02.js
@@ -0,0 +1,9 @@
+function changecolor() {
+let div = document.getElementById("div1_id");
+div.style.color = '#ff0000';
+}
+
+function changetext() {
+    let div = document.getElementById("div1_id");
+    div.className =  'big-text';
+    }
---
+++ JS/try3.js
@@ -0,0 +1,18 @@
+function changeredcolor() {
+  let div = document.getElementById("textchange");
+  div.style.color = "#ff0000";
+  alert("빨간색으로 변경합니다");
+}
+
+function changebluecolor() {
+  let div = document.getElementById("textchange");
+  div.style.color = "#0000ff";
+  alert("파란색으로 변경합니다");
+}
+
+function changegreencolor() {
+  let div = document.getElementById("textchange");
+  div.style.color = "#00ff00";
+  alert("초록색으로 변경합니다");
+}
+
